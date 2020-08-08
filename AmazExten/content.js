faafe5cea59e5928c2ca7e74b469047e1a18ca73
Arseniy <50245554+arseniyy123@users.noreply.github.com>
2020-08-08 19:20:05 +0200

    charitableliving extension

---
+++ AmazExten/content.js
@@ -0,0 +1,7 @@
+//alert("Please visit https://charitableliving.eu/ and get 10% off for all Amazon items")
+
+
+if (window.confirm('If you click "ok" you would be redirected to CharitaBleliving and help people buying Amazon items.')) 
+{
+window.location.href='https://charitableliving.eu/';
+};
